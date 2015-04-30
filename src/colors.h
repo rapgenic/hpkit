5f7fc817e15fefde407d1f1cff65e3bef6340197
Giulio Girardi <giulio@rapgenic.com>
2015-04-30 16:02:25 +0200

    Corrected license information

--- src/colors.h
+++ src/colors.h
@@ -1,8 +1,20 @@
-/* 
- * File:   colors.h
- * Author: giulio
- *
- * Created on 30 aprile 2015, 15.01
+/*
+    Copyright (C) 2015 Giulio Girardi.
+
+    This file is part of HP11em.
+
+    HP11em is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HP11em is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HP11em.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef COLORS_H
